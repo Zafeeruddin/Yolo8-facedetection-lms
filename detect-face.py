c6951b0b8bd8c210720e886ac3ec7378e58f8eff
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-05-21 16:22:36 +0530

    Added same for video

--- detect-face.py
+++ detect-face.py
@@ -3,23 +3,31 @@ import numpy as np
 from main import YOLOv8_face
 import argparse
 import random
+rtsp_url = "rtsp://admin:ADMIN@123@192.168.0.3:554/Streaming/Channels/101"
+cap = cv2.VideoCapture(rtsp_url)
 
-def is_point_in_polygon(point, polygon):
-    return cv2.pointPolygonTest(np.array(polygon, np.int32), point, False) >= 0
+if not cap.isOpened():
+    print("Error: No cam")
+    exit()
 
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
 
-def isPointIn(x,y,dstimg):
+def checkFrameVideo():
+    pass
+
+def isPointIn(x,y,dstimg,polygon):
     print("inside is point")
     h,w,_=dstimg.shape
-    polygon = [(random.randint(0,w),random.randint(0,h)) for _ in range(random.randint(3,10))]
-    polygon = np.array(polygon,np.int32).reshape((-1,1,2))
 
 
     #draw polygon
     cv2.polylines(dstimg,[polygon],isClosed=True,color=(0,255,255),thickness=2)
 
     #check for point
-    inside = is_point_in_polygon((int(x),int(y)),polygon)
+    inside= cv2.pointPolygonTest(np.array(polygon, np.int32), (int(x),int(y)), False) >= 0
+
     print(f"Test point inside: ${inside} ")
     return inside
 
@@ -33,22 +41,29 @@ args = parser.parse_args()
 
 # Initialize YOLOv8_face object detector
 YOLOv8_face_detector = YOLOv8_face(args.modelpath, conf_thres=args.confThreshold, iou_thres=args.nmsThreshold)
-srcimg = cv2.imread(args.imgpath)
 
-# Detect Objects
-boxes, scores, classids, kpts = YOLOv8_face_detector.detect(srcimg)
+
+# Draw polygon
+polygon = [(random.randint(0,w),random.randint(0,h)) for _ in range(random.randint(3,10))]
+polygon = np.array(polygon,np.int32).reshape((-1,1,2))
 
 # Draw detections
-dstimg = YOLOv8_face_detector.draw_detections(srcimg, boxes, scores, kpts)
+while True:
+    ret,frame= cap.read()
+    if not ret:
+        break
+    # srcimg = cv2.imread(frame)
+    # Detect Objects
+    boxes, scores, classids, kpts = YOLOv8_face_detector.detect(frame)
+    dstimg = YOLOv8_face_detector.draw_detections(frame, boxes, scores, kpts)
 
-nose_kp = kpts[0]
-print(kpts)
-x,y,c=nose_kp[0],nose_kp[1],nose_kp[2]
-isPointIn(int(x),int(y),dstimg)
-
-#cv2.imwrite('result.jpg', dstimg)
-winName = 'Deep learning face detection use OpenCV'
-cv2.namedWindow(winName, 0)
-cv2.imshow(winName, dstimg)
+    nose_kp = kpts[0]
+    print(kpts)
+    x,y,c=nose_kp[0],nose_kp[1],nose_kp[2]
+    isPointIn(int(x),int(y),dstimg,polygon)
+    cv2.imwrite('result.jpg', dstimg)
+    winName = 'Deep learning face detection use OpenCV'
+    cv2.namedWindow(winName, 0)
+    cv2.imshow(winName, dstimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
